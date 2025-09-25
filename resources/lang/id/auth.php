83ad2714a90ad7097c1cac2ca2d32be7bb1c3460
nanu <ranusate19@gmail.com>
2025-09-25 14:18:58 +0800

    update

---
+++ resources/lang/id/auth.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'failed' => 'Kredensial ini tidak cocok dengan data kami.',
+    'password' => 'Password yang dimasukkan salah.',
+    'throttle' => 'Terlalu banyak percobaan login. Coba lagi dalam :seconds detik.',
+];
